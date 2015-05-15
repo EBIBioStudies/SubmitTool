66508a5e842cd4ed6ad2a1c17685011b00da02cd
Mike <gostev@ebi.ac.uk>
2015-05-15 16:14:34 +0100

    Separate submission tool project

---
+++ src/main/java/uk/ac/ebi/biostd/tools/submit/Config.java
@@ -0,0 +1,40 @@
+package uk.ac.ebi.biostd.tools.submit;
+
+import java.util.List;
+
+import com.lexicalscope.jewel.cli.Option;
+import com.lexicalscope.jewel.cli.Unparsed;
+
+public interface Config
+{
+  @Unparsed
+   public List<String> getFiles();
+
+  @Option( shortName="i", defaultValue="auto")
+  String getInputFormat();
+  
+  @Option( shortName="u")
+  String getUser();
+
+  @Option( shortName="p")
+  String getPassword();
+  
+  @Option(shortName="s")
+  public String getServer();
+
+  @Option(shortName="l",defaultValue="-")
+  public String getLogFile();
+  
+  @Option(shortName="d")
+  public boolean getPrintInfoNodes();
+  
+  @Option(shortName="o", defaultValue="new")
+  public String getOperation();
+
+  @Option(shortName="c", defaultValue="utf-8")
+  public String getCharset();
+  
+  @Option(helpRequest = true,shortName="h")
+  boolean getHelp();
+  
+}
